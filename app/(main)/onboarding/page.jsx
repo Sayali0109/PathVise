5f29dd8139d72f2547dd397d2378569e2cb12170
Sayali Chaudhari <sayali004@Sayalis-Laptop.local>
2025-03-07 20:40:42 +0530

    changes done

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,13 +1,19 @@
-import { industries } from '@/data/Industries ';
-import React from 'react'
+import { redirect } from "next/navigation";
+import { industries } from "@/data/industries";
+import OnboardingForm from "./_components/onboarding-form";
+import { getUserOnboardingStatus } from "@/actions/user";
 
-const OnBoardingPage = () => {
-  //check if user is already onboarded
+export default async function OnboardingPage() {
+  // Check if user is already onboarded
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
+
   return (
     <main>
-      <OnboardingForm industries = {industries} />
+      <OnboardingForm industries={industries} />
     </main>
-  )
+  );
 }
-
-export default OnBoardingPage;
